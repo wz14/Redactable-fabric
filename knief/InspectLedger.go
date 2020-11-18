06e88bff6fd50f38d02e4d8f65d00633e8cf61e3
Wang Zhuo <zhuowangy2k@outlook.com>
2020-11-18 21:23:48 +0800

    add knief.go file and add some logs

--- knief/InspectLedger.go
+++ knief/InspectLedger.go
@@ -4,27 +4,24 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric-protos-go/common"
 	ab "github.com/hyperledger/fabric-protos-go/orderer"
-	"github.com/hyperledger/fabric/common/flogging"
 	"github.com/hyperledger/fabric/common/ledger/blockledger"
 	"github.com/hyperledger/fabric/common/ledger/blockledger/fileledger"
 	"github.com/hyperledger/fabric/common/metrics/disabled"
 	"github.com/pkg/errors"
 )
 
-var logger = flogging.MustGetLogger("InspectLedger")
-var DataLoc = "./production/orderer"
-var ChannelName = "mychannel"
 // fetch block from dir
 
-func ExtractEnvelopesFromBlock(block *common.Block) (*[]common.Envelope, uint64, error){
+func ExtractEnvelopesFromBlock(block *common.Block) (*[]common.Envelope, uint64, error) {
 	number := len(block.Data.Data)
-	txs := make([]common.Envelope,number)
-	for i:=0;i<number;i++{
-		err := proto.Unmarshal(block.Data.Data[i],&txs[i])
-		if err != nil{
-			return nil, 0, errors.WithMessage(err,"error in parsing the envelop in block")
+	txs := make([]common.Envelope, number)
+	for i := 0; i < number; i++ {
+		err := proto.Unmarshal(block.Data.Data[i], &txs[i])
+		if err != nil {
+			return nil, 0, errors.WithMessage(err, "error in parsing the envelop in block")
 		}
 	}
+	logger.Infof("Extract envelops from %dth block", block.Header.Number)
 	return &txs, uint64(number), nil
 }
 
@@ -39,10 +36,11 @@ func ExtractEnvelope(block *common.Block, index int) (*common.Envelope, error) {
 	}
 	marshaledEnvelope := block.Data.Data[index]
 	envelope, err := GetEnvelopeFromBlock(marshaledEnvelope)
-	if err != nil{
+	if err != nil {
 		err = errors.WithMessagef(err, "block data does not carry an envelope at index %d", index)
 		return nil, err
 	}
+	logger.Infof("Extract %dth envelops from %dth block", index, block.Header.Number)
 	return envelope, nil
 }
 
@@ -58,32 +56,32 @@ func GetEnvelopeFromBlock(data []byte) (*common.Envelope, error) {
 }
 
 // helper function
-func ExtractHeaderFromBlock(block *common.Block) (*common.BlockHeader, error){
+func ExtractHeaderFromBlock(block *common.Block) (*common.BlockHeader, error) {
 	return block.Header, nil
 }
 
-func ExtractDataFromBlock(block *common.Block) (*common.BlockData, error){
+func ExtractDataFromBlock(block *common.Block) (*common.BlockData, error) {
 	return block.Data, nil
 }
 
-func ExtractMetaDataFromBlock(block *common.Block) (*common.BlockMetadata, error){
+func ExtractMetaDataFromBlock(block *common.Block) (*common.BlockMetadata, error) {
 	return block.Metadata, nil
 }
 
-func GetLedgerHeight(fac blockledger.Factory) uint64{
+func GetLedgerHeight(fac blockledger.Factory) uint64 {
 	rw, err := fac.GetOrCreate(ChannelName)
-	if err != nil{
+	if err != nil {
 		logger.Info("factory get channel manager fail")
 	}
 	h := rw.Height()
-	logger.Info("getLedgerHeight:",h)
+	logger.Info("getLedgerHeight:", h)
 	return h
 }
 
-func GetBlockFromNumber(fac blockledger.Factory, num uint64) (*common.Block, error){
-	rw, err:= fac.GetOrCreate(ChannelName)
-	if err != nil{
-		logger.Info("factory get channel manager fail")
+func GetBlockFromNumber(fac blockledger.Factory, num uint64) (*common.Block, error) {
+	rw, err := fac.GetOrCreate(ChannelName)
+	if err != nil {
+		return nil, err
 	}
 	h := rw.Height()
 	if h < num {
@@ -99,10 +97,11 @@ func GetBlockFromNumber(fac blockledger.Factory, num uint64) (*common.Block, err
 	}
 	it, _ := rw.Iterator(seekPosition)
 	block, _ := it.Next()
-	return block,nil
+	logger.Infof("get %dth block from ledger", num)
+	return block, nil
 }
 
-func GetFactory() (blockledger.Factory, string, error){
+func GetFactory() (blockledger.Factory, string, error) {
 	metricProv := disabled.Provider{}
 	ld := DataLoc
 	logger.Info("Ledger dir:", ld)
@@ -115,16 +114,16 @@ func GetFactory() (blockledger.Factory, string, error){
 
 func SerializeBlock(block *common.Block) ([]byte, error) {
 	blockbytes, err := proto.Marshal(block)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return blockbytes, nil
 }
 
-func DeserializeBlock(blk []byte) (*common.Block, error){
+func DeserializeBlock(blk []byte) (*common.Block, error) {
 	b := common.Block{}
 	err := proto.Unmarshal(blk, &b)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return &b, nil
---
+++ knief/knief.go
@@ -0,0 +1,7 @@
+package knief
+
+import "github.com/hyperledger/fabric/common/flogging"
+
+var logger = flogging.MustGetLogger("knief")
+var DataLoc = "./production/orderer"
+var ChannelName = "mychannel"
